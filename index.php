672694c0a6640d361cf2bf9fdb9b90b1177d5682
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2022-10-03 15:05:59 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -8,8 +8,34 @@
 <body>
   <p>
     <?php
-    echo "bonjour je suis Vadim";
+		$nom = 'Vadim';
+		$passion_1 = 'Programmation';
+		$passion_2 = 'Voyage';
+		$passion_3 = 'Cyclisme';
+		$calcul = 3*3*3*3*3;
+
+
+    echo "<h1>Bonjour je suis $nom</h1>";
+		echo "<h2>J'ai 19 ans et bientôt 20</h2>";
+		echo "<p>Mes passions sont :</p>";
+		echo "<ul>";
+		echo "<li>$passion_1</li>";
+		echo "<li>$passion_2</li>";
+		echo "<li>$passion_3</li>";
+		echo "</ul>";
+		echo "<p>3 puissance 5, ça fait : $calcul</p>";
+
+		$str= strtoupper($nom);
+		echo "<p>$str</p>";
+
+		$my_password='Vadim2003';
+
+		echo password_hash("$my_password", PASSWORD_DEFAULT);
+
      ?>
+
+
+
   </p>
 
 </body>
