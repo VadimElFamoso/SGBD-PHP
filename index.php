5d1cbc6d7298642caf578c3c356c7b4f1eea8484
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2022-10-03 14:36:44 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta name=description content="cardiff">
+	<title>Essai PHP</title>
+	<link rel="stylesheet" href="styles.css" type="text/css" media="screen" />
+</head>
+<body>
+  <p>
+    <?php
+    echo "bonjour je suis Vadim";
+     ?>
+  </p>
+
+</body>
+</html>
