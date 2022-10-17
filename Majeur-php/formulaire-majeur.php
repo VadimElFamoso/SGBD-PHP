2b14cc057e5e6f7f9f491735fb7f722fe7acce5e
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2022-10-17 16:05:24 +0200

    Vérification de la majorité via formulaire et php

---
+++ Majeur-php/formulaire-majeur.php
@@ -0,0 +1,11 @@
+<?php
+$age_legal = 18;
+$age_utilisateur = $_POST['age'];
+if($age_utilisateur>=18){
+  echo "<h1>Etant majeur ($age_utilisateur ans), vous pouvez ouvrir un compte en banque.</h1>";
+}
+
+else{
+  echo "<h1>Désolé, mais vous êtes trop jeune pour ouvrir un compte en banque ! La majorité est fixé à $age_legal ans ! </h1>";
+}
+ ?>
