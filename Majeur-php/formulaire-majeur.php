15693748e563b454a220ec224031d6efd36e1ad9
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2022-10-17 16:06:52 +0200

    Formulaire de vérification de la majorité

--- Majeur-php/formulaire-majeur.php
+++ Majeur-php/formulaire-majeur.php
@@ -1,7 +1,7 @@
 <?php
 $age_legal = 18;
 $age_utilisateur = $_POST['age'];
-if($age_utilisateur>=18){
+if($age_utilisateur>=$age_legal){
   echo "<h1>Etant majeur ($age_utilisateur ans), vous pouvez ouvrir un compte en banque.</h1>";
 }
 
