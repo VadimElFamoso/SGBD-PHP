820bc891a1136ce7f16fc68ab6e76c3d9596473b
Vadim2k <44468431+VadimElFamoso@users.noreply.github.com>
2022-10-24 15:51:42 +0200

    Add files via upload

--- emprunt.php
+++ emprunt.php
@@ -3,9 +3,12 @@ $emprunt_value = $_POST['emprunt_value'];
 $remboursement_value = $_POST['remboursement_value'];
 
 $i=1;
-while($emprunt_value>=0){
+while($emprunt_value>0){
   echo "Mois $i, ce mois-ci vous deviez $emprunt_value, vous avez remboursé $remboursement_value ce mois-ci !<br>";
   $emprunt_value = $emprunt_value - $remboursement_value;
   $i++;
+  //Rajouter un test pour vérifier si on rembourse trop, il faut rembourser ce qu'il reste.
+  //Rajouter un taux d'intérêt au formulaire php
+  //Rajouter le déjà payé.
 }
  ?>
